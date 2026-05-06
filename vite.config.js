71ef9df52ae3255d9a65a2f81c67d278fa89321c
Filip Bergander <filip.bergander1996@gmail.com>
2026-05-06 10:24:24 +0200

    first commit

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+    css: {
+        devSourcemap: true
+    },
+    build: {
+        rollupOptions: {
+            input: {
+                index: resolve(__dirname, "index.html")
+            }
+        }
+    }
+});
